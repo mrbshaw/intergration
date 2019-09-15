3892c6b089761a0af5c8fe1a8c9fd27fb0f1adbb
Robert Shaw <54412628+mrbshaw@users.noreply.github.com>
2019-09-15 12:34:40 -0400

    Update Main.py
    Added in the Details.  Found out what kind of trivia game I wanted.

--- Main.py
+++ Main.py
@@ -1,4 +1,10 @@
 #Robert Shaw
-#A quiz game
-print("Welcome to my my passion")
+#trivia game based of the Wheel of time books
+print("Welcome to My Trivia Game!")
+firstNumber=input("Enter a number to find your lucky number!: ")
+secondNumber=input("Enter Another Number: ")
+num1 = int(firstNumber)
+num2 = int(secondNumber)
+subt = num1 - num2
+print("Based on the numbers you entered, your lucky number is:", + subt)
 
