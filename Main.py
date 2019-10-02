7f9d403f804944cfe876f902034a0e1c08fd53f3
Robert Shaw <54412628+mrbshaw@users.noreply.github.com>
2019-10-02 18:28:38 -0400

    Update Main.py
    Added a While loop Statement for when asking for lucky number. So now the user can only enter numbers 1 through 10 and if anything else is imputed, it will give an error and ask them to enter the number again. It will keep looping until the correct range of numbers is entered.

--- Main.py
+++ Main.py
@@ -1,10 +1,22 @@
 #Robert Shaw
 #trivia game based of the Wheel of time books
 print("Welcome to My Trivia Game!")
-firstNumber=input("Enter a number to find your lucky number!: ")
-secondNumber=input("Enter Another Number: ")
-num1 = int(firstNumber)
-num2 = int(secondNumber)
+
+while True:
+    firstNumber = input("Enter a number between 1 and 10 to find your lucky number!: ")
+    num1 = int(firstNumber)
+    if num1 <= 10 and num1 >= 0:
+        break
+    else:
+        print("Number out of range, please try again")
+
+while True:
+    secondNumber=input("Enter Another Number between 1 and 10: ")
+    num2 = int(secondNumber)
+    if num2 <= 10 and num2 >= 0:
+        break
+    else:
+        print("Number out of range, please try again")
 subt = num1 - num2
 print("Based on the numbers you entered, your lucky number is:", + subt)
 
