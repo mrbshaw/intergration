b5102b2aab2a43da083a9fc2f87ba930a286bd30
Robert Shaw <54412628+mrbshaw@users.noreply.github.com>
2019-10-11 19:31:06 -0400

    Update Main.py
    Made nested while loops and added the first two questions. Also added accumulator to keep track of score.

--- Main.py
+++ Main.py
@@ -27,3 +27,30 @@ while start:
         start = False
 
 print('Here we go!')
+#gameRun to restart the again when finished
+gameRun= True
+while gameRun:
+    playerScore = 0
+    quest1= True
+    while quest1:
+        qu1= input("Question1: How many WOT books are there?" + "\n[a] 1 " + "\n[b] 5" +"\n[c] 15" + "\n[d] 10" + "\nAnswer: ")
+        if qu1 != 'c':
+            print("Sorry that is incorrect.")
+        elif qu1 == 'c':
+            playerScore += 1
+            print("Correct!"+ " " + "your Score:", playerScore ,"out of 10")
+            quest1 = False
+
+    quest2= True
+    while quest2:
+        qu2= input("Question2: What is the title of book one?" + "\n[a] Wheel of Time" + "\n[b] Mars Attacks" + "\n[c] MistBorn" + "\n[d] The Way of Kings"+ "\nAnswer: ")
+        if qu2 != 'a':
+            print("sorry that is incorrect.")
+        elif qu2 == 'a':
+            playerScore += 1
+            print("Correct!" + " " + "your Score:", playerScore , "out of 10")
+            quest2 = False
+
+    quest3= True
+    qu3= input("Question3: ")
+
